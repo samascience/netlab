d85aa8a29b435df14fca95b688293803cc350b30
Lou DeVictoria <47089483+telescope40@users.noreply.github.com>
2024-11-12 15:46:55 +0000

    Feature policy (#5) * Add README and setup.sh
    * Update Images
    
    * Update
    
    * update
    
    * Update README
    
    * Add Policy Lab
    
    * challenge 2
    
    * Update Challenge2
    
    * Update Challenge2

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 sudo apt-get update && sudo apt-get install -y python3-pip
 python3 -m pip install networklab
 netlab install -y ubuntu ansible containerlab libvirt
+echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
+tee -a /etc/apt/sources.list.d/netdevops.list -y
+apt install containerlab -y
+apt-get install containerd -y
+apt remove moby-tini -y
+apt-get install pip -y
+pip install -r requirements.txt
 apt install graphviz
